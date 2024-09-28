1c81fd7b85905596c4080883e251f88727b225fa
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-09-27 22:53:40 -0400

    Devek

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { ReactNode, useState } from 'react';
-import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, Box } from '@mui/material';
 import { Menu as MenuIcon, Upload as UploadIcon, List as ListIcon, ExitToApp as LogoutIcon, Login as LoginIcon } from '@mui/icons-material';
 import Link from 'next/link';
 
@@ -28,13 +28,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   ];
 
   return (
-    <div>
-      <AppBar position="static">
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+            sx={{ mr: 2 }}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             File Proxy
           </Typography>
           {isLoggedIn && (
@@ -44,20 +50,33 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           )}
         </Toolbar>
       </AppBar>
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={drawerOpen}
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+          },
+        }}
+      >
+        <Toolbar /> {/* This empty Toolbar acts as a spacer */}
         <List>
           {isLoggedIn ? (
             menuItems.map((item) => (
-              <Link href={item.href} key={item.text} passHref>
-                <ListItem button onClick={toggleDrawer}>
+              <Link href={item.href} key={item.text} passHref legacyBehavior>
+                <ListItem button component="a" onClick={toggleDrawer}>
                   <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItem>
               </Link>
             ))
           ) : (
-            <Link href="/login" passHref>
-              <ListItem button onClick={toggleDrawer}>
+            <Link href="/login" passHref legacyBehavior>
+              <ListItem button component="a" onClick={toggleDrawer}>
                 <ListItemIcon><LoginIcon /></ListItemIcon>
                 <ListItemText primary="Login" />
               </ListItem>
@@ -65,8 +84,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           )}
         </List>
       </Drawer>
-      <main>{children}</main>
-    </div>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - 240px)` } }}>
+        <Toolbar /> {/* This empty Toolbar acts as a spacer */}
+        {children}
+      </Box>
+    </Box>
   );
 };
 
