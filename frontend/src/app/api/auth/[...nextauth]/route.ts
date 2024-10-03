38edad74b89f45e83551b750783d2e3b064c56f5
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-03 10:18:20 -0400

    Enable next-auth beta

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,2 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
 export const { GET, POST } = handlers
-
-import NextAuth from "next-auth"
-   import GoogleProvider from "next-auth/providers/google"
-
-   export default NextAuth({
-     providers: [
-       GoogleProvider({
-         clientId: process.env.GOOGLE_CLIENT_ID,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-       }),
-     ],
-     // Add any additional configuration options here
-   })
