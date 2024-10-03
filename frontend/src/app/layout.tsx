29c0bba65c1ade16680e35632cc4da43d3ec3125
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-03 18:33:31 -0400

    next-auth

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '@/components/Layout';
+import { getServerSession } from "next-auth/next"
+import SessionProvider from "@/components/SessionProvider"
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body>
+      <SessionProvider session={session}>
           <Layout>{children}</Layout>
+        </SessionProvider>
       </body>
     </html>
   );
