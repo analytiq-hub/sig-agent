510a101e53c158557c684f5d173f9216287261ae
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-05 21:57:20 -0400

    Devel

--- frontend/src/lib/mongodb.ts
+++ frontend/src/lib/mongodb.ts
@@ -1,5 +1,6 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient, ServerApiVersion } from "mongodb"
+import { MongoClient } from "mongodb"
+// import {ServerApiVersion} from "mongodb"
  
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
@@ -7,11 +8,11 @@ if (!process.env.MONGODB_URI) {
  
 const uri = process.env.MONGODB_URI
 const options = {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  // serverApi: {
+  //   version: ServerApiVersion.v1,
+  //   strict: true,
+  //   deprecationErrors: true,
+  // },
 }
  
 let client: MongoClient
@@ -30,6 +31,7 @@ if (process.env.NODE_ENV === "development") {
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
+  
 }
  
 // Export a module-scoped MongoClient. By doing this in a
