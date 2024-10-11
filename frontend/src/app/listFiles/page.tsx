c87210f989bc1353d14cc92ef5877ed13c30a6bf
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-11 16:12:05 -0400

    Devel

--- frontend/src/app/listFiles/page.tsx
+++ frontend/src/app/listFiles/page.tsx
@@ -6,7 +6,7 @@ import Test from '@/components/ListFiles';
 const ListFilesPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">List Files Page</h1>
+      <h1 className="text-2xl font-bold mb-4">File List</h1>
       <Test />
     </div>
   );
