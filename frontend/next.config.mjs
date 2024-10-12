81b161d67f94a212748a8b0b2ffb8362789266ad
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-12 13:51:31 -0400

    Devel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//import { withPlugins } from 'next-compose-plugins';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const nextConfig = {
+  webpack: (config) => {
+    if (config.optimization) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            // Your Terser options here
+          },
+          exclude: /pdf.worker.min.mjs/,
+        }),
+      ];
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
