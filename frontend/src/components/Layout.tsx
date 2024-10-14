381d4bd40e707e92176ffe7f61865e769c7f2f9c
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-14 00:06:04 -0400

    Devel

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -214,7 +214,8 @@ export default function Layout({ children }: { children: ReactNode }) {
           {debugMenuItems.map(item => renderMenuItem(item, open))}
         </List>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` }, marginBottom: 2 }}>
+      {/* p is for padding */}
+      <Box sx={{ flexGrow: 1, p: 0, width: { sm: `calc(100% - ${drawerWidth}px)` }, marginBottom: 2 }}>
         <DrawerHeader />
         {children}
       </Box>
