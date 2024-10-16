25ae62b62bba792b8100501c2b61ce30833b5c96
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-16 00:00:07 -0400

    Devel

--- frontend/src/app/models/page.tsx
+++ frontend/src/app/models/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import React from 'react';
-import Flows from '@/components/Flows';
+import Models from '@/components/Models';
 
-const FlowsPage: React.FC = () => {
+const ModelsPage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Flows</h1>
-      <Flows />
+      <h1 className="text-xl font-bold mb-4">Models</h1>
+      <Models />
     </div>
   );
 };
 
-export default FlowsPage;
+export default ModelsPage;
