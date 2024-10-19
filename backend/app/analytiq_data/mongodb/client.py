c92d39f2ed8043b34ecfdb25e6dbea9fe8ed1f96
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-18 21:59:10 -0400

    Devel

--- backend/app/analytiq_data/mongodb/client.py
+++ backend/app/analytiq_data/mongodb/client.py
@@ -11,8 +11,5 @@ def get_mongodb_client():
     Get the MongoDB client.
     """
     mongo_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-    env = os.getenv("ENV", "dev")
-    if env == "prod" and mongo_uri == "mongodb://localhost:27017":
-        raise ValueError("MONGODB_URI is not set for prod")
     client = AsyncIOMotorClient(mongo_uri)
-    return client[env]
+    return client
