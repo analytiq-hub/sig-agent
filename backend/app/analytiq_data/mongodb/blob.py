cac1afd618d9eb5730544a369db6fedeeb7ccbb2
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-19 07:10:30 -0400

    Devel

---
+++ backend/app/analytiq_data/mongodb/blob.py
@@ -0,0 +1,101 @@
+import gridfs
+from datetime import datetime, UTC
+import os
+
+import analytiq_data as ad
+
+def get_blob(analytiq_client, bucket: str, key: str) -> dict:
+    """
+    Get the file
+    
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        bucket : str
+            bucket name
+        key : str
+            blob key
+
+    Returns:
+        dict
+            {"blob": bytes, "metadata": dict}
+    """
+    # Get the provider db
+    mongo = analytiq_client.mongodb
+    db_name = analytiq_client.env
+    db = mongo[db_name]
+    collection = db[f"{bucket}.files"]
+
+    # Get the doc metadata
+    elem = collection.find_one({"name": key})
+    if elem is None:
+        return None
+    metadata = elem["metadata"]
+    
+    # Get the blob
+    fs = gridfs.GridFS(db, collection=bucket)
+    elem = fs.find_one({"name": key})
+    blob = elem.read()
+
+    blob_dict = {
+        "blob": blob,
+        "metadata": metadata
+    }
+
+    return blob_dict
+
+def save_blob(analytiq_client, bucket:str, key:str, blob:bytes, metadata:dict):
+    """
+    Save the file
+    
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        bucket : str
+            bucket name
+        key : str
+            blob key
+        blob : bytes
+            blob blob
+        metadata : dict
+            blob metadata
+    """
+    # Get the db
+    mongo = analytiq_client.mongodb
+    db_name = analytiq_client.env
+    db = mongo[db_name]
+    fs = gridfs.GridFS(db, collection=bucket)
+
+    # Remove the old file
+    try:
+        file = fs.find_one({"name": key})
+        fs.delete(file._id)
+        ad.log.debug(f"Blob {bucket}/{key} has been deleted.")
+    except:
+        pass
+
+    fs.put(blob, name=key, metadata=metadata)
+    ad.log.debug(f"Blob {bucket}/{key} has been saved.")
+
+def delete_blob(analytiq_client, bucket:str, key:str):
+    """
+    Delete the blob
+
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        bucket : str
+            bucket name
+        key : str
+            blob key
+    """
+    # Get the db
+    mongo = analytiq_client.mongodb
+    db_name = analytiq_client.env
+    db = mongo[db_name]
+    fs = gridfs.GridFS(db, collection=bucket)
+
+    # Remove the old blob
+    blob = fs.find_one({"name": key})
+    fs.delete(blob._id)
+    ad.log.debug(f"Blob {bucket}/{key} has been deleted.")
