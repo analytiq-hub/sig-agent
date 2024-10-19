3336d368ce39db7f3822f77aaa6ebe24061488d8
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-18 22:01:19 -0400

    Devel

--- backend/app/analytiq_data/common/client.py
+++ backend/app/analytiq_data/common/client.py
@@ -1,11 +1,18 @@
 import analytiq_data as ad
 
 class AnalytiqClient:
-    def __init__(self):
+    def __init__(self, env: str = "dev"):
+        self.env = env
         self.mongodb = ad.mongodb.get_mongodb_client()
 
-def get_client() -> AnalytiqClient:
+def get_client(env: str = "dev") -> AnalytiqClient:
     """
     Get the AnalytiqClient.
+
+    Args:
+        env: The environment to connect to. Defaults to "dev".
+
+    Returns:
+        The AnalytiqClient.
     """
-    return AnalytiqClient()
+    return AnalytiqClient(env)
