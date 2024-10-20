95667cec75f05ecde2ec40df1a0a3b810beb93fa
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-19 22:46:22 -0400

    Devel

--- backend/worker/worker.py
+++ backend/worker/worker.py
@@ -16,27 +16,21 @@ ad.common.setup()
 ad.init_logger("worker")
 
 # Environment variables
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 ENV = os.getenv("ENV", "dev")
+
+client = ad.common.get_client(env=ENV)
+db_name = "prod" if ENV == "prod" else "dev"
+db = client.mongodb_async[db_name]
+job_queue_collection = db.job_queue
 
 async def process_job(job):
     # Implement your job processing logic here
     print(f"Processing job: {job['_id']}")
     # Simulate work
     await asyncio.sleep(10)
-    await queue_collection.update_one({"_id": job["_id"]}, {"$set": {"status": "completed"}})
+    await job_queue_collection.update_one({"_id": job["_id"]}, {"$set": {"status": "completed"}})
 
 async def worker():
-    client = ad.common.get_client(env=ENV)
-    db_name = "prod" if ENV == "prod" else "dev"
-    db = client.mongodb_async[db_name]
-    job_queue_collection = db.job_queue
-
-    ad.log.info(f"Connected to MongoDB at {MONGODB_URI} and using database {db_name}")
-
-    # List the collections in the database
-    ad.log.info(f"Collections in database {db_name}: {await db.list_collection_names()}")
-
     while True:
         job = await job_queue_collection.find_one_and_update(
             {"status": "pending"},
