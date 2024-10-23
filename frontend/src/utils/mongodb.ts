26d07636e9b671081c46060dae00865c9fba9b05
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-10-23 18:16:02 -0400

    Devel

--- frontend/src/utils/mongodb.ts
+++ frontend/src/utils/mongodb.ts
@@ -1,11 +1,15 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb"
  
+if (!process.env.ENV) {
+  throw new Error('Invalid/Missing environment variable: "ENV"')
+}
+
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
  
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI + "/" + process.env.ENV
 const options = {
 }
  
