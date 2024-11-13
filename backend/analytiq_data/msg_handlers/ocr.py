e1d1d5c1d78275046e732280c1df085ec8db7814
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-13 09:20:14 -0500

    devel

--- backend/analytiq_data/msg_handlers/ocr.py
+++ backend/analytiq_data/msg_handlers/ocr.py
@@ -4,6 +4,9 @@ import analytiq_data as ad
 async def process_ocr_msg(analytiq_client, msg):
     # Implement your job processing logic here
     ad.log.info(f"Processing OCR msg: {msg}")
+
+    document_id = msg["document_id"]
+
     # Simulate work
-    await asyncio.sleep(10)
+    await asyncio.sleep(1)
     await ad.queue.delete_msg(analytiq_client, "ocr", msg["_id"])
