19fa6b84d44936af49b72a0a6cf6bd73c74c28e6
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-13 09:33:12 -0500

    devel

--- backend/analytiq_data/common/ocr.py
+++ backend/analytiq_data/common/ocr.py
@@ -23,19 +23,19 @@ def get_ocr_json(analytiq_client, document_id: str) -> dict:
     return ad.mongodb.get_blob(analytiq_client, bucket=OCR_BUCKET, key=key)
    
 
-def save_ocr_json(analytiq_client, document_id:str, ocr_json:dict, metadata:dict):
+def save_ocr_json(analytiq_client, document_id:str, ocr_json:dict, metadata:dict=None):
     """
     Save the OCR JSON
     
     Args:
         analytiq_client: AnalytiqClient
             The analytiq client
-        file_name : str
-            file name
-        blob : bytes
-            file blob
+        document_id : str
+            document id
+        ocr_json : dict
+            OCR JSON
         metadata : dict
-            file metadata
+            OCR metadata
     """
     key = f"{document_id}_json"
     ad.mongodb.save_blob(analytiq_client, bucket=OCR_BUCKET, key=key, blob=ocr_json, metadata=metadata)
@@ -44,15 +44,79 @@ def save_ocr_json(analytiq_client, document_id:str, ocr_json:dict, metadata:dict
 
 def delete_ocr_json(analytiq_client, document_id:str):
     """
-    Delete the file
+    Delete the OCR JSON
 
     Args:
         analytiq_client: AnalytiqClient
             The analytiq client
-        file_name : str
-            File name
+        document_id : str
+            document id
     """
     key = f"{document_id}_json"
     ad.mongodb.delete_blob(analytiq_client, bucket=OCR_BUCKET, key=key)
 
     ad.log.debug(f"OCR JSON for {document_id} has been deleted.")
+
+def get_ocr_text(analytiq_client, document_id:str, page_idx:int=None) -> str:
+    """
+    Get the OCR text
+    
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        document_id : str
+            document id
+        page_idx : int
+            page index. If None, return the whole OCR text.
+
+    Returns:
+        str
+            OCR text
+    """
+    key = f"{document_id}_text"
+    if page_idx is not None:
+        key += f"_page_{page_idx}"
+    return ad.mongodb.get_blob(analytiq_client, bucket=OCR_BUCKET, key=key)
+   
+
+def save_ocr_text(analytiq_client, document_id:str, ocr_text:str, page_idx:int=None, metadata:dict=None):
+    """
+    Save the OCR text
+    
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        document_id : str
+            document id
+        ocr_text : str
+            OCR text
+        page_idx : int
+            page index
+        metadata : dict
+            OCR metadata
+    """
+    key = f"{document_id}_text"
+    if page_idx is not None:
+        key += f"_page_{page_idx}"
+    ad.mongodb.save_blob(analytiq_client, bucket=OCR_BUCKET, key=key, blob=ocr_text, metadata=metadata)
+    
+    ad.log.debug(f"OCR text for {document_id} page {page_idx} has been saved.")
+
+def delete_ocr_text(analytiq_client, document_id:str, page_idx:int=None):
+    """
+    Delete the OCR text
+
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        document_id : str
+            document id
+        page_idx : int
+            page index
+    """
+    key = f"{document_id}_text"
+    if page_idx is not None:
+        key += f"_page_{page_idx}"
+    ad.mongodb.delete_blob(analytiq_client, bucket=OCR_BUCKET, key=key)
+
+    ad.log.debug(f"OCR text for {document_id} page {page_idx} has been deleted.")
