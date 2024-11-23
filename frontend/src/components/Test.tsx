e845fdb479aca172cb23e63d7a45bf4f6d807d38
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-22 23:52:23 -0500

    Devel

--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -9,43 +9,28 @@ const Test: React.FC = () => {
         <nav className="flex justify-between items-center sticky top-0 z-10 py-4 bg-blue-900">
           <div className="flex-shrink-0 ml-6 cursor-pointer">
             <FontAwesomeIcon icon={faWind} className="text-2xl text-yellow-500" />
-            <span className="text-3xl font-semibold text-blue-200">Tailwind School</span>
+            <span className="text-xl font-semibold text-blue-200">Smart Document Router</span>
           </div>
-          <ul className="flex mr-10 font-semibold">
+          <ul className="flex mr-10 font-semibold-sm">
             <li className="mr-6 p-1 border-b-2 border-yellow-500">
-              <a className="cursor-default text-blue-200" href="#">Home</a>
+              <a className="cursor-default text-blue-200" href="#">Dashboard</a>
             </li>
             <li className="mr-6 p-1 border-b-2 border-yellow-500">
-              <a className="text-white hover:text-blue-300" href="#">News</a>
+              <a className="text-white hover:text-blue-300" href="#">Upload</a>
             </li>
             <li className="mr-6 p-1">
-              <a className="text-white hover:text-blue-300" href="#">Tutorials</a>
+              <a className="text-white hover:text-blue-300" href="#">List Files</a>
             </li>
             <li className="mr-6 p-1">
-              <a className="text-white hover:text-blue-300" href="#">Videos</a>
+              <a className="text-white hover:text-blue-300" href="#">Models</a>
+            </li>
+            <li className="mr-6 p-1">
+              <a className="text-white hover:text-blue-300" href="#">Flows</a>
             </li>
           </ul>
         </nav>
       </div>
 
-      <div class="mx-auto p-8 w-full lg:w-1/2">
-
-      <h1 class="text-3xl font-bold">Grid Layout Examples</h1>
-
-      <h2 class="my-6 text-2xl underline underline-offset-2">Example #1</h2>
-
-      <div class="grid grid-rows-3 grid-cols-3 gap-x-4 gap-y-2">
-        <div class="box col-span-2 bg-green-500">1</div>
-        <div class="box row-span-2 bg-green-500">2</div>
-        <div class="box row-span-2 bg-green-500">3</div>
-        <div class="box bg-green-500">4</div>
-        <div class="box col-span-2 bg-green-500">5</div>
-
-
-      </div>
-
-
-    </div>
     </div>
   );
 };
