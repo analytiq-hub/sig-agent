7f40850978de90d14a91f5f6b17736e48d1cf0ca
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-22 16:25:17 -0800

    Devel

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -5,10 +5,11 @@ import Test from '@/components/Test';
 
 const TestPage: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Test Page</h1>
-      <Test />
-    </div>
+    // <div className="container mx-auto p-4">
+    //   <h1 className="text-xl font-bold mb-4">Test Page</h1>
+    //   <Test />
+    // </div>
+    <Test />
   );
 };
 
--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWind } from '@fortawesome/free-solid-svg-icons';
 
 const Test: React.FC = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className="container mx-auto">
+        <nav className="flex justify-between items-center sticky top-0 z-10 py-4 bg-blue-900">
+          <div className="flex-shrink-0 ml-6 cursor-pointer">
+            <FontAwesomeIcon icon={faWind} className="text-2xl text-yellow-500" />
+            <span className="text-3xl font-semibold text-blue-200">Tailwind School</span>
+          </div>
+          <ul className="flex mr-10 font-semibold">
+            <li className="mr-6 p-1 border-b-2 border-yellow-500">
+              <a className="cursor-default text-blue-200" href="#">Home</a>
+            </li>
+            <li className="mr-6 p-1">
+              <a className="text-white hover:text-blue-300" href="#">News</a>
+            </li>
+            <li className="mr-6 p-1">
+              <a className="text-white hover:text-blue-300" href="#">Tutorials</a>
+            </li>
+            <li className="mr-6 p-1">
+              <a className="text-white hover:text-blue-300" href="#">Videos</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      
+      <nav className="flex items-center justify-between p-4">
+        <div>
+          <h1>Logo</h1>
+        </div>
+        
+        <div> 
+          <ul className="flex mr-5">
+            <li className="mr-6 p-1">Nav Links</li>
+            <li className="mr-6 p-1">Nav Links</li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
