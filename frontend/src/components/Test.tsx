01a49de203e43dd351b719563e43801903272e20
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-24 14:10:11 -0500

    devel

--- frontend/src/components/Test.tsx
+++ frontend/src/components/Test.tsx
@@ -4,7 +4,7 @@ import { faWind } from '@fortawesome/free-solid-svg-icons';
 
 const Test: React.FC = () => {
   return (
-    <div>
+    <div className="antialiased min-h-screen relative">
       <nav className="w-full sticky top-0 z-10 bg-blue-900">
         <div className="container mx-auto flex justify-between items-center py-4">
           <div className="flex-shrink-0 ml-6 cursor-pointer">
@@ -30,6 +30,26 @@ const Test: React.FC = () => {
           </ul>
         </div>
       </nav>
+      <nav className="absolute z-10 w-80 bg-indigo-900 text-white h-screen p-3">
+        <div className="flex justify-between">
+          <span className="font-bold text-xl sm:text-2xl p-2">Sidebar</span>
+          <button className="p-2 focus:outline-none ml-auto focus:bg-indigo-800 hover:bg-indigo-800 rounded-md">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+        </div>
+        <ul className="mt-8">
+          <li className="block px-4 py-2 hover:bg-indigo-800 rounded-md">
+            <a href="#" className="flex items-center">Home</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="relative z-0"> 
+        <header className="bg-gray-700">
+          <span className="block text-white text-xl sm:text-2xl font-bold p-4">Smart Document Router</span>
+        </header>
+      </div>
     </div>
   );
 };
