dd9bb6e767644f88a0da8760c8585f5a462b17c0
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-26 13:03:21 -0500

    devel

--- backend/analytiq_data/msg_handlers/ocr.py
+++ backend/analytiq_data/msg_handlers/ocr.py
@@ -36,6 +36,9 @@ async def process_ocr_msg(analytiq_client, aws_client, msg, force:bool=False):
         if file is None:
             ad.log.error(f"File for {document_id} not found. Skipping OCR.")
             return
+
+        # TO DO: read from config the OCR type, and run tesseract OCR instead of Textract
+        
         # Run OCR
         ocr_list = ad.aws.textract.run_textract(aws_client, file["blob"])
         ad.log.info(f"OCR completed for {document_id}")
