6c43edd929c8d3616af1dcf4fbbcc66a494787d6
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-28 09:48:24 -0500

    devel

--- backend/analytiq_data/aws/client.py
+++ backend/analytiq_data/aws/client.py
@@ -20,32 +20,44 @@ class AWSClient:
             aws_secret_access_key=self.aws_secret_access_key
         )
 
-        # Get the user's identity
-        user_identity = self.user_session.client("sts").get_caller_identity()
+        # It's possible that the AWS keys are not set, in which case
+        # assuming the role will fail.
+        # Initialize the AWS clients with neutral values, and set them only if the
+        # credentials are valid.
+        self.session = self.user_session
+        self.s3 = None
+        self.textract = None
 
-        # Get the assume role ARN
-        assume_role_arn = get_assume_role_arn(user_identity["Arn"])
+        try:
+            # Get the user's identity
+            user_identity = self.user_session.client("sts").get_caller_identity()
 
-        fetcher = AssumeRoleCredentialFetcher(
-            client_creator=self.user_session.client,
-            source_credentials=self.user_session.get_credentials(),
-            role_arn=assume_role_arn,
-        ) 
-        botocore_session = botocore.session.Session()
-        botocore_session._credentials = DeferredRefreshableCredentials(
-            method='assume-role',
-            refresh_using=fetcher.fetch_credentials
-        )
+            # Get the assume role ARN
+            assume_role_arn = get_assume_role_arn(user_identity["Arn"])
 
-        # Create the assumed role session
-        self.session = boto3.Session(botocore_session=botocore_session)
+            fetcher = AssumeRoleCredentialFetcher(
+                client_creator=self.user_session.client,
+                source_credentials=self.user_session.get_credentials(),
+                role_arn=assume_role_arn,
+            ) 
+            botocore_session = botocore.session.Session()
+            botocore_session._credentials = DeferredRefreshableCredentials(
+                method='assume-role',
+                refresh_using=fetcher.fetch_credentials
+            )
 
-        # Create the s3 client
-        self.s3 = self.session.client("s3", region_name=region_name)
-        self.s3_bucket_name = "analytiq-data"
+            # Create the assumed role session
+            self.session = boto3.Session(botocore_session=botocore_session)
 
-        # Create the textract client
-        self.textract = self.session.client("textract", region_name=region_name)
+            # Create the s3 client
+            self.s3 = self.session.client("s3", region_name=region_name)
+            self.s3_bucket_name = "analytiq-data"
+
+            # Create the textract client
+            self.textract = self.session.client("textract", region_name=region_name)
+        except Exception as e:
+            ad.log.info(f"AWS credentials are not correct: {e}")
+            ad.log.info("AWS client created with empty AWS credentials")
 
 def get_aws_client(analytiq_client, region_name: str = "us-east-1") -> AWSClient:
     """
@@ -74,12 +86,17 @@ def get_aws_keys(analytiq_client) -> dict:
     aws_keys_collection = db["aws_credentials"]
 
     aws_keys = aws_keys_collection.find_one()
-    if aws_keys is None or "access_key_id" not in aws_keys or "secret_access_key" not in aws_keys:
-        raise ValueError("AWS keys not found")
+    
+    # Parse the AWS keys
+    access_key_id = ""
+    secret_access_key = ""
+    if aws_keys:
+        access_key_id = aws_keys.get("access_key_id", "")
+        secret_access_key = aws_keys.get("secret_access_key", "")
 
     return {
-        "aws_access_key_id": aws_keys["access_key_id"],
-        "aws_secret_access_key": aws_keys["secret_access_key"]
+        "aws_access_key_id": access_key_id,
+        "aws_secret_access_key": secret_access_key
     }
 
 def get_assume_role_arn(user_arn: str) -> str:
