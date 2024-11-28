077a7d91616c7c8da007dbd21006784d7a320990
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-11-28 09:39:40 -0500

    devel

--- backend/analytiq_data/mongodb/client.py
+++ backend/analytiq_data/mongodb/client.py
@@ -17,8 +17,6 @@ def get_mongodb_client_async(env: str = "dev") -> AsyncIOMotorClient:
         The MongoDB client.
     """
     mongo_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-    if env == "prod" and mongo_uri == "mongodb://localhost:27017":
-        raise ValueError("MONGODB_URI is not set for prod")
     client = AsyncIOMotorClient(mongo_uri)
     return client
 
@@ -33,8 +31,6 @@ def get_mongodb_client(env: str = "dev") -> MongoClient:
         The MongoDB client.
     """
     mongo_uri = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-    if env == "prod" and mongo_uri == "mongodb://localhost:27017":
-        raise ValueError("MONGODB_URI is not set for prod")
     client = MongoClient(mongo_uri,
                          w='majority', # write operations will wait for a majority of the replica set members to acknowledge the write before returning successfully
                          retryWrites=False, # retryWrites not supported in AWS DocumentDB
