9e4beccdef7a2dd64bc4818a1e926551822e7a58
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-12-03 22:46:11 -0500

    devel

--- frontend/src/app/settings/user/developer/page.tsx
+++ frontend/src/app/settings/user/developer/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 
 import React from 'react';
-import AccessTokenManager from '@/components/AccessTokenManager';
 import SettingsLayout from '@/components/SettingsLayout';
+import Link from 'next/link';
+import { Button } from '@mui/material';
 
 const DeveloperPage: React.FC = () => {
   return (
     <SettingsLayout selectedMenu="user_developer">
-      <AccessTokenManager />
+      <div className="space-y-6">
+        <div className="flex justify-between items-center mb-4">
+          <div>
+            <h2 className="text-xl font-semibold">Access tokens</h2>
+            <p className="text-gray-600 mb-2">
+              Use Access Tokens to authenticate with our API.
+            </p>
+          </div>
+          <Link href="/settings/user/developer/access-tokens" passHref>
+            <Button variant="contained" color="primary">
+              Manage
+            </Button>
+          </Link>
+        </div>
+      </div>
     </SettingsLayout>
   );
 };
