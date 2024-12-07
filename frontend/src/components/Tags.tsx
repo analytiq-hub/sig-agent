46bbb4338815ca6e3b7d1c083ef7a3a5c64c5f0e
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-12-07 17:31:39 -0500

    Tags configuration

--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -11,9 +11,9 @@ import colors from 'tailwindcss/colors';
 
 const isColorLight = (color: string): boolean => {
   const hex = color.replace('#', '');
-  const r = parseInt(hex.substr(0, 2), 16);
-  const g = parseInt(hex.substr(2, 2), 16);
-  const b = parseInt(hex.substr(4, 2), 16);
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 2), 16);
+  const b = parseInt(hex.substring(4, 2), 16);
   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
   return brightness > 128;
 };
