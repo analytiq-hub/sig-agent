28629ee19b50b1d5ba6d42a14a4682464c23c55e
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-12-08 23:20:45 -0500

    Update the document state

--- backend/analytiq_data/msg_handlers/llm.py
+++ backend/analytiq_data/msg_handlers/llm.py
@@ -4,15 +4,27 @@ import analytiq_data as ad
 
 
 async def process_llm_msg(analytiq_client, msg):
-    # Implement your LLM job processing logic here
     ad.log.info(f"Processing LLM msg: {msg}")
 
-    llm_key = await ad.llm.get_llm_key(analytiq_client)
-
-    # Get the entire document text from MongoDB
     document_id = msg["msg"]["document_id"]
+    
+    try:
+        # Update state to LLM processing
+        await ad.common.doc.update_doc_state(analytiq_client, document_id, ad.common.doc.DOCUMENT_STATE_LLM_PROCESSING)
 
-    await ad.llm.run_llm(analytiq_client, document_id)
+        llm_key = await ad.llm.get_llm_key(analytiq_client)
+        await ad.llm.run_llm(analytiq_client, document_id)
 
-    ad.log.info(f"LLM run completed for {document_id}")
+        # Update state to LLM completed
+        await ad.common.doc.update_doc_state(analytiq_client, document_id, ad.common.doc.DOCUMENT_STATE_LLM_COMPLETED)
+        
+        ad.log.info(f"LLM run completed for {document_id}")
+    except Exception as e:
+        ad.log.error(f"Error processing LLM msg: {e}")
+        
+        # Update state to LLM failed
+        await ad.common.doc.update_doc_state(analytiq_client, document_id, ad.common.doc.DOCUMENT_STATE_LLM_FAILED)
+        
+        # Could add LLM error queue handling here if needed
+        
     await ad.queue.delete_msg(analytiq_client, "llm", msg["_id"])
