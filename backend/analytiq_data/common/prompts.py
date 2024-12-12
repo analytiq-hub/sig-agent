85f82602cbfb2383d52ef8b3422a7b50d3feaa5b
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-12-11 22:09:31 -0500

    devel

--- backend/analytiq_data/common/prompts.py
+++ backend/analytiq_data/common/prompts.py
@@ -4,6 +4,28 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 import analytiq_data as ad
 
+async def get_prompt_id(analytiq_client, prompt_name: str) -> str:
+    """
+    Get a prompt ID by its name
+
+    Args:
+        analytiq_client: AnalytiqClient
+            The analytiq client
+        prompt_name: str
+            Prompt name
+
+    Returns:
+        str
+            Prompt ID
+    """
+    db_name = analytiq_client.env
+    db = analytiq_client.mongodb_async[db_name]
+    collection = db["prompts"]
+    elem = await collection.find_one({"name": prompt_name})
+    if elem is None:
+        raise ValueError(f"Prompt {prompt_name} not found")
+    return str(elem["_id"])
+
 
 async def get_prompt(analytiq_client, prompt_id: str) -> dict:
     """
