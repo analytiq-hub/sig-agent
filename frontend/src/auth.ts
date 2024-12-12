60e9623d24278f5ca6f6ec919ba0c88b4270fb80
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2024-12-12 16:13:48 -0500

    Fix dr names

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -67,6 +67,7 @@ export const authOptions: NextAuthOptions = {
                     prompt: "consent",
                     access_type: "offline",
                     response_type: "code",
+                    scope: "openid email profile"
                 }
             }
         })
@@ -154,6 +155,35 @@ export const authOptions: NextAuthOptions = {
             }
             
             return session as AppSession;
+        }
+    },
+    useSecureCookies: process.env.NODE_ENV === "production",
+    cookies: {
+        sessionToken: {
+            name: process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token",
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production"
+            }
+        },
+        callbackUrl: {
+            name: process.env.NODE_ENV === "production" ? "__Secure-next-auth.callback-url" : "next-auth.callback-url",
+            options: {
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production"
+            }
+        },
+        csrfToken: {
+            name: process.env.NODE_ENV === "production" ? "__Host-next-auth.csrf-token" : "next-auth.csrf-token",
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production"
+            }
         }
     }
 };
