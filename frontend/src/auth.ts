dcbe695caaad8f1efce6acc2245497c3bc19c0b3
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-02-22 20:45:24 -0500

    devel

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -217,6 +217,10 @@ export const authOptions: NextAuthOptions = {
     },
     pages: {
         signIn: '/auth/signin',
+        //signOut: '/auth/signout',
+        //error: '/auth/error',
+        //verifyRequest: '/auth/verify-request',
+        //newUser: '/auth/new-user'
     },
 };
 
