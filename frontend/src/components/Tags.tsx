2d293b21fcfa54f8089f830f49b26e68e4a38222
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-05-13 22:04:00 -0400

    devel

--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -97,7 +97,10 @@ const Tags: React.FC<{ organizationId: string }> = ({ organizationId }) => {
         const textColor = isColorLight(bgColor) ? 'text-gray-800' : 'text-white';
         
         return (
-          <div className="flex items-center h-full w-full">
+          <div 
+            className="flex items-center h-full w-full cursor-pointer"
+            onClick={() => handleEdit(params.row)}
+          >
             <div 
               className={`px-2 py-1 leading-none rounded shadow-sm ${textColor}`}
               style={{ 
