f77466f6051818f98673fa2b3a683695424a00b2
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-07-09 09:24:28 -0400

    devel

---
+++ public/js/calendly.js
@@ -0,0 +1,4 @@
+function openCalendly() {
+    // Open Calendly in a new window/tab to avoid cookie and CSP issues
+    window.open('https://calendly.com/analytiqhub', '_blank', 'width=800,height=600,scrollbars=yes,resizable=yes');
+} 
