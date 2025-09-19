7ed202771d8e0952ece36137f984a91234e6a35d
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-09-19 17:17:28 -0400

    Optimize async

--- packages/analytiq_data/common/file.py
+++ packages/analytiq_data/common/file.py
@@ -119,40 +119,6 @@ def download_file(analytiq_client, file_name: str, output_path: str):
     with open(output_path, "wb") as file:
         file.write(file["blob"])
 
-
-# Download all the files from the database
-def download_all_files(analytiq_client, output_dir: str):
-    """
-    Download all the files from the database
-
-    Args:
-        analytiq_client: AnalytiqClient
-            The analytiq client
-        output_dir : str
-            Output directory
-    """
-    # Get the db
-    mongo = analytiq_client.mongodb
-    db_name = analytiq_client.env
-    db = mongo[db_name]
-    collection = db["files.files"]
-
-    # Get all the files
-    cursor = collection.find()
-    files = cursor.to_list(length=None)
-
-    # Download each file
-    for file in files:
-        file_name = file["name"]
-        file = get_file(analytiq_client, file_name)
-        file_blob = file["blob"]
-        file_metadata = file["metadata"]
-
-        # Save the file to the output directory
-        file_path = os.path.join(output_dir, file_name)
-        with open(file_path, "wb") as file:
-            file.write(file_blob)
-
 async def get_file_async(analytiq_client, file_name: str) -> dict:
     """
     Get the file asynchronously
