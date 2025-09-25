6aee667595ac362286ce929ba8217cba97d3caf1
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-09-25 07:50:59 +0300

    devel

--- packages/tests/docrouter_sdk/test_docrouter_client.py
+++ packages/tests/docrouter_sdk/test_docrouter_client.py
@@ -13,7 +13,7 @@ import logging
 from datetime import datetime, UTC
 
 # Import shared test utilities
-from .conftest_utils import (
+from tests.conftest_utils import (
     client, TEST_ORG_ID, 
     get_auth_headers
 )
