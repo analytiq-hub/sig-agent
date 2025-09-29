a7d15cc09ddcd5e8b3183077db1eaa547d85cd04
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-09-29 07:56:46 -0400

    devel

--- packages/docrouter_sdk/src/docrouter_sdk/__init__.py
+++ packages/docrouter_sdk/src/docrouter_sdk/__init__.py
@@ -1,3 +1,12 @@
 from .client import DocRouterClient
 
-__all__ = ["DocRouterClient"]
+try:
+    from importlib.metadata import version, PackageNotFoundError
+    try:
+        __version__ = version("docrouter-sdk")
+    except PackageNotFoundError:
+        __version__ = "0.0.0"
+except Exception:
+    __version__ = "0.0.0"
+
+__all__ = ["DocRouterClient", "__version__"]
