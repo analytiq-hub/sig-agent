550789d1180a69a9143a13f2b3160037e11d5a83
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-10-02 15:38:51 -0400

    devel

--- packages/docrouter_ts_sdk/jest.config.js
+++ packages/docrouter_ts_sdk/jest.config.js
@@ -1,16 +1,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/tests/**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  // Only run setup for integration tests
+  setupFilesAfterEnv: process.env.TEST_TYPE === 'integration' ? ['<rootDir>/tests/setup/jest-setup.ts'] : [],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
     '!src/__tests__/**',
+    '!tests/**',
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
+  testTimeout: process.env.TEST_TYPE === 'integration' ? 60000 : 5000, // 60s for integration, 5s for unit
+  ...(process.env.TEST_TYPE === 'integration' && { maxWorkers: 1 }), // Sequential for integration tests
 };
