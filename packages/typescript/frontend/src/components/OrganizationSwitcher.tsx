d28f7b0e3d2efd45a299af53c53e544f3b7c7116
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2025-10-18 20:10:40 -0400

    Fix workspace dropdown

--- packages/typescript/frontend/src/components/OrganizationSwitcher.tsx
+++ packages/typescript/frontend/src/components/OrganizationSwitcher.tsx
@@ -85,7 +85,7 @@ export default function OrganizationSwitcher() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none max-h-80 overflow-y-auto">
           <div className="py-1">
             {organizations.map((organization) => (
               <Menu.Item key={organization.id}>
